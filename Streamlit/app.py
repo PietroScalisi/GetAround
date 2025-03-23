1e1883fa644c9af2ee157edac2911e0f8aba164e
PietroScalisi <pietroscalisi97@gmail.com>
2025-03-23 15:39:22 +0100

    Modify differents files

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -28,7 +28,7 @@ def charger_donnees_excel():
     """
     Charge les données Excel et retourne un DataFrame
     """
-    donnees_excel = pd.read_excel(r"C:\Users\pietr\OneDrive\Desktop\FullStack\GetAround\API\get_around_delay_analysis.xlsx")
+    donnees_excel = pd.read_excel(r"get_around_delay_analysis.xlsx")
     return donnees_excel
 
 # Charger les données des deux fichiers
